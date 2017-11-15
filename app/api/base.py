ecbdab1eb3e9168bf059765502e54b17d2b9240e
Adam West <jsarry@gmail.com>
2017-11-15 13:57:16 +0200

    Api module - register login post and get data

---
+++ app/api/base.py
@@ -0,0 +1,52 @@
+import requests
+from config.config import Config
+
+
+class ApiBase(object):
+    _base_url = 'http://sensor.app/api/'
+    _api_version = 'v1'
+    _jwt_token = ''
+    _id = ''
+
+    def __init__(self):
+        self._config = Config()
+
+    def refresh_token(self):
+        pass
+
+    # Post data to server
+    #
+    # endpoint(string) : required
+    #       relative endpoint url
+    # payload (dictionary) : re
+    def post(self, endpoint, payload=None, json=None):
+        endpoint_url = self._base_url + endpoint
+
+        try:
+            if json:
+                r = requests.post(endpoint_url,json=json)
+            else:
+                r = requests.post(endpoint_url, data=payload)
+            r.raise_for_status()
+            return r.json()
+        except requests.RequestException as e:
+            print('Request failed: {}'.format(e))
+            raise
+
+    def get(self, endpoint):
+        endpoint_url = self._base_url + endpoint
+        try:
+            r = requests.get(endpoint_url).raise_for_status()
+            r = requests.get(endpoint_url).json()
+            return r
+        except requests.RequestException as e:
+            print('Request failed: {}'.format(e))
+            raise
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def jwt_token(self):
+        return self._jwt_token
